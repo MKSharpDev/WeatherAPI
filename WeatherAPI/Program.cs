6636637557fd43afa0c9d3153ca1abe2f7f01bc2
MegoTelega <MegoTelega@DESKTOP-AIQ1L00>
2023-04-06 15:27:32 +0300

    Добавьте файлы проекта.

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System.Net;
+using System.Text.Json.Nodes;
+using WeatherAPI;
+using Newtonsoft.Json;
+
+string appIdKey = File.ReadAllText("key.txt").ToString();
+// You need to register on the site openweathermap.org and get the key in the section "My API keys". 
+string url = $"https://api.openweathermap.org/data/2.5/weather?lat=44.58&lon=33.5224&units=metric&appid={appIdKey}";
+
+
+HttpWebRequest resquest = (HttpWebRequest)WebRequest.Create(url);
+
+HttpWebResponse response = (HttpWebResponse)resquest.GetResponse();
+
+string rezult;
+using (StreamReader str = new StreamReader(response.GetResponseStream()))
+{
+    rezult = str.ReadToEnd();
+}
+
+ Weather weather = JsonConvert.DeserializeObject<Weather>(rezult);
+
+Console.WriteLine($" Whether in {weather.Name} : {weather.Main.Temp} degrees Celcius, feels like {weather.Main.Feels_like}; wind speed {weather.Wind.Speed} ");
---
+++ WeatherAPI/Weather.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAPI
+{
+    public class Weather
+    {
+        public Temperature Main { get; set; }
+        public Wind Wind { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class Temperature
+    {
+        public double Temp { get; set; }
+        public double Feels_like { get; set; }
+    }
+
+    public class Wind
+    {
+        public double Speed { get; set; }
+    }
+
+
+}
